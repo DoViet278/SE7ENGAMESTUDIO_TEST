44a4286d9206e7a39e6bf7298b0ce8839ffc33ee
DoViet278 <dohuuviet278@gmail.com>
2026-05-13 17:00:12 +0700

    add wall collider

--- Assets/Scripts/PlayController.cs
+++ Assets/Scripts/PlayController.cs
@@ -6,8 +6,6 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
 
     [Header("Field Limit")]
-    public Vector2 xLimit = new Vector2(-20, 20);
-    public Vector2 zLimit = new Vector2(-10, 10);
 
     private CharacterController controller;
     private Animator animator;
@@ -40,9 +38,6 @@ public class PlayerController : MonoBehaviour
         }
 
         Vector3 pos = transform.position;
-
-        pos.x = Mathf.Clamp(pos.x, xLimit.x, xLimit.y);
-        pos.z = Mathf.Clamp(pos.z, zLimit.x, zLimit.y);
 
         transform.position = pos;
 
